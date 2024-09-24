a8f0cec5566aab91e112d5bffb52b12ca4bdb81f
Gustavo Rodrigues Santos Silva <gusantos@foursys.com.br>
2024-09-24 17:58:32 -0300

    refatorando errorResponse

--- Sistema-de-Controle-de-Notas/src/main/java/br/com/gustavorssbr/Sistema/de/Controle/de/Notas/utils/ErrorResponseFactory.java
+++ Sistema-de-Controle-de-Notas/src/main/java/br/com/gustavorssbr/Sistema/de/Controle/de/Notas/utils/ErrorResponseFactory.java
@@ -3,7 +3,6 @@ package br.com.gustavorssbr.Sistema.de.Controle.de.Notas.utils;
 import br.com.gustavorssbr.Sistema.de.Controle.de.Notas.config.dto.ErrorAutenticacaoResponseDTO;
 
 public class ErrorResponseFactory {
-
     public static ErrorAutenticacaoResponseDTO createResponseError(String message, String path, Integer httpStatus) {
         return new ErrorAutenticacaoResponseDTO(message, path, httpStatus);
     }
